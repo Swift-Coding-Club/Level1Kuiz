6ba360773b7d6ad4fb74806034a0b7c4f09b5127
백은주 <specialguest@live.co.kr>
2022-09-09 15:37:41 +0900

    Initial commit

---
+++ Level1Kuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Level1Kuiz
+//
+//  Created by 백은주 on 2022/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Level1Kuiz/Level1KuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Level1KuizApp.swift
+//  Level1Kuiz
+//
+//  Created by 백은주 on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct Level1KuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
