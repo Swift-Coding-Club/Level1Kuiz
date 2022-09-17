b2cb26d76b10344bf05b9433dfcb76874289f53c
Kwon Seung Yong <ericyong95@gmail.com>
2022-09-17 14:06:57 +0900

    feat: Sample

---
+++ Level1Kuiz/Seunyong/Sample.swift
@@ -0,0 +1,20 @@
+//
+//  Sample.swift
+//  Level1Kuiz
+//
+//  Created by 권승용 on 2022/09/17.
+//
+
+import SwiftUI
+
+struct Sample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Sample_Previews: PreviewProvider {
+    static var previews: some View {
+        Sample()
+    }
+}
