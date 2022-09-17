b10c8099255efff5b41efb16caebd0312ae25d7b
HamSik <109560875+SukHyeon@users.noreply.github.com>
2022-09-17 14:23:14 +0900

    feat: Sample

---
+++ Level1Kuiz/Hamsik/Sample.swift
@@ -0,0 +1,20 @@
+//
+//  Sample.swift
+//  Level1Kuiz
+//
+//  Created by 황석현 on 2022/09/17.
+//
+
+import SwiftUI
+
+struct Sample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Sample_Previews: PreviewProvider {
+    static var previews: some View {
+        Sample()
+    }
+}
