601d21a5c17239c25c38ee9c68410037b9435fc9
KSYong <ericyong95@naver.com>
2022-09-28 11:36:50 +0900

    feat: KuizView 기본적인 로직 완성

--- Level1Kuiz/KuizView/KuizView.swift
+++ Level1Kuiz/KuizView/KuizView.swift
@@ -9,27 +9,63 @@ import SwiftUI
 
 struct KuizView: View {
 
+    @State private var correctCount: Int = 0
+    @State private var currentCount: Int = 0
+
+    let sampleData = DataManager.share.getData()
+
+    @State private var randomFlag: Int = Int.random(in: 0...1)
+
     var body: some View {
 
-        VStack(spacing: 20) {
-            Label("현재 문재 몇 번째인지 알려주기", systemImage: "circle" )
-            Text("다음 중 맞는 것을 고르세요!")
-            Button {
+        VStack(spacing: 50) {
+            
+            Text(String(correctCount) + "/" + String(sampleData.count))
+            VStack(spacing: 20) {
 
-            } label: {
-                HStack {
-                    Text("placeholder")
+                Text("다음 중 맞는 것을 고르세요!")
+                
+                if let quizString = sampleData[currentCount].quizString {
+                    Text(quizString)
                 }
-            }
-            Button {
+                
+                if randomFlag == 0 {
+                    HStack {
+                        Button {
+                            randomFlag = Int.random(in: 0...1)
+                            correctCount += 1
+                            currentCount += 1
+                        } label: {
+                            Text(sampleData[currentCount].correctAnswer)
+                        }
 
-            } label: {
-                HStack {
-                    Text("placeholder")
+                        Button {
+                            randomFlag = Int.random(in: 0...1)
+                            currentCount += 1
+                        } label: {
+                            Text(sampleData[currentCount].wrongAnswer)
+                        }
+                    }
+                } else {
+                    HStack {
+                        Button {
+                            randomFlag = Int.random(in: 0...1)
+                            currentCount += 1
+                        } label: {
+                            Text(sampleData[currentCount].wrongAnswer)
+                        }
+
+                        Button {
+                            randomFlag = Int.random(in: 0...1)
+                            correctCount += 1
+                            currentCount += 1
+                        } label: {
+                            Text(sampleData[currentCount].correctAnswer)
+                        }
+                    }
                 }
-            }
 
-        
+            }
         }
     }
 }
