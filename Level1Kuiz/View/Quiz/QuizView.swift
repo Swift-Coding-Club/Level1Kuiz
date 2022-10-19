653064f1d94363e41920ec8686b77ae1cc653190
KSYong <ericyong95@naver.com>
2022-10-19 16:46:58 +0900

    fix: 퀴즈뷰 수정사항 구현 완료

--- Level1Kuiz/View/Quiz/QuizView.swift
+++ Level1Kuiz/View/Quiz/QuizView.swift
@@ -8,126 +8,136 @@
 import SwiftUI
 
 struct QuizView: View {
-    // PresentationMode는 Deprecated 될 예정이라 추후 수정 필요
+
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     @GestureState private var dragOffSet = CGSize.zero
 
-    //    @StateObject private var quizModel = QuizModel()
-
     @State var quizzes: [Quiz] = []
+    @State var randomQuizIndexes: [Int] = [0, 1]
     @State var currentCount: Int = 0
-    @State var correctCount: Int = 0
-
+    @State var didGetQuizWrong: Bool = false
     @State var isQuizEnded: Bool = false
 
     @Binding var isNavigationLinkActive: Bool
 
-    /**
-     API 요청 한계를 넘겨 호출이 되지 않기 때문에
-     임시 데이터 사용
-     */
-    @State var sampleQuizzes: [Quiz] = DataManager.share.getQuizzes()
-
     var body: some View {
+
         GeometryReader { geometry in
-            VStack {
-                Spacer()
-                    .frame(height: 100)
-                Text(" 다음 중 맞는 것을 고르세요!")
-                    .font(.title)
-                    .fontWeight(.heavy)
-                Text("\(currentCount) / \(sampleQuizzes.count)")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .padding(EdgeInsets(top: 1, leading: 0, bottom: 20, trailing: 0))
-                VStack(alignment: .center, spacing: 20) {
+            ZStack {
+                Color.yellow
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
                     Spacer()
-                    if !isQuizEnded {
-                        Label(sampleQuizzes[currentCount].answers[0].text, systemImage: "circle")
-                            .frame(maxWidth: geometry.size.width * 0.8)
-                            .font(Font.title3.bold())
-                            .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
-                            .background(.white)
-                            .cornerRadius(40, antialiased: true)
-                            .shadow(radius: 1)
-                            .onTapGesture {
-                                if sampleQuizzes[currentCount].answers[0].isCorrect {
-                                    correctCount += 1
-                                }
-                                currentCount += 1
-                                if currentCount == sampleQuizzes.count {
-                                    isQuizEnded = true
-                                }
-                            }
-                        Label(sampleQuizzes[currentCount].answers[1].text, systemImage: "circle")
-                            .frame(maxWidth: geometry.size.width * 0.8)
-                            .font(Font.title3.bold())
-                            .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
-                            .background(.white)
-                            .cornerRadius(40, antialiased: true)
-                            .shadow(radius: 1)
-                            .onTapGesture {
-                                if sampleQuizzes[currentCount].answers[0].isCorrect {
-                                    correctCount += 1
-                                }
-                                currentCount += 1
-                                if currentCount == sampleQuizzes.count {
-                                    isQuizEnded = true
-                                }
-                            }
-                    } else {
-                        Text("모든 문제가 끝났습니다!")
-                            .font(Font.title.bold())
-                    }
+                        .frame(height: geometry.size.height * 0.05)
+                    Text("\(currentCount)")
+                        .font(Font.system(size: 75).bold())
+                        .fontWeight(.bold)
                     Spacer()
-                    if isQuizEnded {
-                        NavigationLink(destination: ResultView(isNavigationLinkActive: $isNavigationLinkActive)) {
-                            Text("결과 보기")
-                                .font(.system(size: 20))
-                                .fontWeight(.black)
-                                .frame(width: 160, height: 60)
-                                .foregroundColor(Color.white)
-                                .background(Color.black)
-                                .cornerRadius(80)
-                        }
-                    } else {
-                        HStack(spacing: 20) {
-                            Button {
+                        .frame(height: geometry.size.height * 0.08)
+                    ZStack {
+                        Color.white
+                            .cornerRadius(60, corners: [.topLeft, .topRight])
+                            .edgesIgnoringSafeArea(.bottom)
+                        VStack(alignment: .center, spacing: 20) {
+                            if !quizzes.isEmpty {
+                                if !isQuizEnded && !didGetQuizWrong {
+                                    Text(quizzes[currentCount].answers[randomQuizIndexes[0]].text)
+                                        .frame(minWidth: geometry.size.width * 0.7)
+                                        .font(Font.title3.bold())
+                                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
+                                        .background(.white)
+                                        .cornerRadius(40, antialiased: true)
+                                        .shadow(radius: 1)
+                                        .onTapGesture {
+                                            if quizzes[currentCount].answers[randomQuizIndexes[0]].isCorrect {
+                                                currentCount += 1
+                                            } else {
+                                                // 문제 틀리면 결과 화면으로..
+                                                didGetQuizWrong = true
+                                            }
+                                            if currentCount == quizzes.count {
+                                                isQuizEnded = true
+                                            }
+                                            randomQuizIndexes.shuffle()
+                                        }
+                                    Text(quizzes[currentCount].answers[randomQuizIndexes[1]].text)
+                                        .frame(minWidth: geometry.size.width * 0.7)
+                                        .font(Font.title3.bold())
+                                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
+                                        .background(.white)
+                                        .cornerRadius(40, antialiased: true)
+                                        .shadow(radius: 1)
+                                        .onTapGesture {
+                                            if quizzes[currentCount].answers[randomQuizIndexes[1]].isCorrect {
+                                                currentCount += 1
+                                            } else {
+                                                // 문제 틀리면 결과 화면으로..
+                                                didGetQuizWrong = true
+                                            }
+                                            if currentCount == quizzes.count {
+                                                isQuizEnded = true
+                                            }
+                                            randomQuizIndexes.shuffle()
+                                        }
+                                } else if !isQuizEnded && didGetQuizWrong {
+                                    VStack(spacing: 50) {
+                                        Text("앗! 문제를 틀렸습니다.")
+                                            .font(Font.title.bold())
+                                        NavigationLink(destination: ResultView(isNavigationLinkActive: $isNavigationLinkActive)) {
+                                            Text("결과 보기")
+                                                .font(.system(size: 20))
+                                                .fontWeight(.black)
+                                                .frame(width: 160, height: 60)
+                                                .foregroundColor(Color.white)
+                                                .background(Color.black)
+                                                .cornerRadius(80)
 
-                            } label: {
-                                Text("모르겠어요!")
-                                    .font(.system(size: 20))
-                                    .fontWeight(.black)
-                                    .frame(width: 160, height: 60)
-                                    .foregroundColor(Color.white)
-                                    .background(Color.black)
-                                    .cornerRadius(80)
-                            }
-
-                            NavigationLink(destination: ResultView(isNavigationLinkActive: $isNavigationLinkActive)) {
-                                Text("결과 보기")
-                                    .font(.system(size: 20))
-                                    .fontWeight(.black)
-                                    .frame(width: 160, height: 60)
-                                    .foregroundColor(Color.white)
-                                    .background(Color.black)
-                                    .cornerRadius(80)
+                                        }
+                                    }
+                                } else if isQuizEnded && !didGetQuizWrong {
+                                    VStack(spacing: 50) {
+                                        Text("모든 문제가 끝났습니다!")
+                                            .font(Font.title.bold())
+                                        NavigationLink(destination: ResultView(isNavigationLinkActive: $isNavigationLinkActive)) {
+                                            Text("결과 보기")
+                                                .font(.system(size: 20))
+                                                .fontWeight(.black)
+                                                .frame(width: 160, height: 60)
+                                                .foregroundColor(Color.white)
+                                                .background(Color.black)
+                                                .cornerRadius(80)
+                                        }
+                                    }
+                                }
+                            } else {
+                                ProgressView()
+                                    .progressViewStyle(CircularProgressViewStyle(tint: .black))
+                                    .scaleEffect(2)
                             }
                         }
+                        // VStack
+                        .onAppear {
+                            QuizService().getQuizzes { result in
+                                switch result {
+                                case .success(let data):
+                                    quizzes = data.shuffled()
+                                case .failure(let error):
+                                    print(error)
+                                }
+                            }
+                            randomQuizIndexes.shuffle()
+                        }
                     }
-                    Spacer()
-                        .frame(height: 24)
+                    // ZStack
+                    .frame(maxWidth: .infinity)
                 }
-                .padding(.bottom, 30)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.white)
-                .cornerRadius(60, corners: [.topLeft, .topRight])
-                //                .shadow(radius: 5)
+                // VStack
             }
-            .ignoresSafeArea()
+            // ZStack
         }
-        .background(Color.yellow)
+        // GeometryReader
         // 커스텀 뒤로가기 버튼 만들기
+        .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: Button(action: {
             self.mode.wrappedValue.dismiss()
@@ -141,12 +151,9 @@ struct QuizView: View {
                 self.mode.wrappedValue.dismiss()
             }
         }))
-        .onAppear {
-            //            quizzes = quizModel.data
-            //            sampleQuizzes = dataManager.getQuizzes()
-        }
         .preferredColorScheme(.light)
     }
+
 }
 
 struct QuizView_Previews: PreviewProvider {
