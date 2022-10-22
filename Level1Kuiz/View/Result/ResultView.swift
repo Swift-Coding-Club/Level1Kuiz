333b13de8099a34fa50c95fbeee84d7419c1e28e
HamSik <109560875+SukHyeon@users.noreply.github.com>
2022-10-22 13:15:07 +0900

    feat: 홈버튼 활성화

--- Level1Kuiz/View/Result/ResultView.swift
+++ Level1Kuiz/View/Result/ResultView.swift
@@ -8,40 +8,47 @@
 import SwiftUI
 
 struct ResultView: View {
-    
+
     @Binding var isNavigationLinkActive: Bool
     @State var userName: String = ""
-    
+
     var body: some View {
         VStack {
             Text("퀴즈가 끝났어요!")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .padding()
-            
+
             Text("점수는 0")
                 .font(.title2)
                 .fontWeight(.heavy)
                 .padding()
-            
+
             HStack(alignment: .center, spacing: 10) {
                 TextField("저장하시겠습니까?", text: $userName)
                     .frame(width: 150, height: 30, alignment: .center)
                 Button {
                 } label: {
                     Text("저장하기")
+                        .font(.system(size: 17))
                 }
             }.padding()
-            
+
             NavigationLink(destination: QuizView(isNavigationLinkActive: $isNavigationLinkActive), isActive: $isNavigationLinkActive) {
                 Button {
                     isNavigationLinkActive = false
                 } label: {
-                    Text("Home")
+                    Text("홈으로 가기")
+                        .font(.system(size: 20))
+                        .fontWeight(.black)
+                        .frame(width: 160, height: 60)
+                        .foregroundColor(Color.white)
+                        .background(Color.black)
+                        .cornerRadius(80)
                 }
-                
+
             }
-            
+
         }
     }
 }
