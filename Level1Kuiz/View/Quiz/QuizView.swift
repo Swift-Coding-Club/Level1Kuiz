3be339f6378c62114bbf13311ad7abcd4307208f
백은주 <specialguest@kakao.com>
2022-12-03 04:01:01 +0900

    feat: QuizView 레이아웃 재구성

--- Level1Kuiz/View/Quiz/QuizView.swift
+++ Level1Kuiz/View/Quiz/QuizView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftUIMargin
 
 struct QuizView: View {
 
@@ -13,139 +14,90 @@ struct QuizView: View {
     @GestureState private var dragOffSet = CGSize.zero
 
     @State var quizzes: [Quiz] = []
-    @State var randomQuizIndexes: [Int] = [0, 1]
-    @State var currentCount: Int = 0
-    @State var didGetQuizWrong: Bool = false
+    @State var score: Int = 0
+    @State var answer: String = ""
+    @State var description: String = ""
+    @State var randomBool: Bool = Bool.random()
     @State var isQuizEnded: Bool = false
 
-    @Binding var isNavigationLinkActive: Bool
-    @State var isShowingResultView = false
-
     var body: some View {
-
         GeometryReader { geometry in
             ZStack {
-                NavigationLink(destination: ResultView(isNavigationLinkActive: $isNavigationLinkActive, score: currentCount), isActive: $isShowingResultView) {}
+                NavigationLink(
+                    destination: ResultView(
+                        score: score,
+                        answer: answer,
+                        description: description
+                    ),
+                    isActive: $isQuizEnded
+                ) {}
                     .hidden()
 
                 Color.yellow
                     .edgesIgnoringSafeArea(.all)
 
                 VStack {
-                    Spacer()
-                        .frame(height: geometry.size.height * 0.05)
-                    Text("\(currentCount)")
-                        .font(Font.system(size: 75).bold())
+                    Text("\(score)")
+                        .font(Font.system(size: 80))
                         .fontWeight(.bold)
-                    Spacer()
-                        .frame(height: geometry.size.height * 0.08)
-                    ZStack {
-                        Color.white
-                            .cornerRadius(60, corners: [.topLeft, .topRight])
-                            .edgesIgnoringSafeArea(.bottom)
-                        VStack(alignment: .center, spacing: 20) {
-                            if !quizzes.isEmpty {
 
-                                Text(quizzes[currentCount].answers[randomQuizIndexes[0]].text)
-                                    .frame(minWidth: geometry.size.width * 0.7)
-                                    .font(Font.title2.bold())
-                                    .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
-                                    .background(.white)
-                                    .cornerRadius(40, antialiased: true)
-                                    .shadow(radius: 1)
-                                    .onTapGesture {
-                                        if quizzes[currentCount].answers[randomQuizIndexes[0]].isCorrect {
-                                            currentCount += 1
-                                        } else {
-                                            // 문제 틀리면 결과 화면으로..
-                                            didGetQuizWrong = true
-                                            if !isQuizEnded && didGetQuizWrong {
-                                                isShowingResultView = true
-                                            }
-                                            if isQuizEnded && !didGetQuizWrong {
-                                                isShowingResultView = true
-                                            }
-                                        }
-                                        if currentCount == quizzes.count {
-                                            isQuizEnded = true
-                                        }
-                                        randomQuizIndexes.shuffle()
-                                    }
-                                Text(quizzes[currentCount].answers[randomQuizIndexes[1]].text)
-                                    .frame(minWidth: geometry.size.width * 0.7)
-                                    .font(Font.title2.bold())
-                                    .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
-                                    .background(.white)
-                                    .cornerRadius(40, antialiased: true)
-                                    .shadow(radius: 1)
-                                    .onTapGesture {
-                                        if quizzes[currentCount].answers[randomQuizIndexes[1]].isCorrect {
-                                            currentCount += 1
-                                        } else {
-                                            // 문제 틀리면 결과 화면으로..
-                                            didGetQuizWrong = true
-                                            if !isQuizEnded && didGetQuizWrong {
-                                                isShowingResultView = true
-                                            }
-                                            if isQuizEnded && !didGetQuizWrong {
-                                                isShowingResultView = true
-                                            }
-                                        }
-                                        if currentCount == quizzes.count {
-                                            isQuizEnded = true
-                                        }
-                                        randomQuizIndexes.shuffle()
-                                    }
-
-                            } else {
-                                ProgressView()
-                                    .progressViewStyle(CircularProgressViewStyle(tint: .black))
-                                    .scaleEffect(2)
-                            }
-                        }
-                        // VStack
-                        .onAppear {
-                            QuizService().getQuizzes { result in
-                                switch result {
-                                case .success(let data):
-                                    quizzes = data.shuffled()
-                                case .failure(let error):
-                                    print(error)
-                                }
-                            }
-                            randomQuizIndexes.shuffle()
+                    VStack(alignment: .center, spacing: 12) {
+                        if quizzes.isEmpty {
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle(tint: .black))
+                                .scaleEffect(2)
+                        } else {
+                            generateAnswerText(index: Int(randomBool), geometry: geometry)
+                            generateAnswerText(index: Int(!randomBool), geometry: geometry)
                         }
                     }
-                    // ZStack
-                    .frame(maxWidth: .infinity)
+                    .onAppear {
+                        QuizService().getQuizzes { result in
+                            switch result {
+                            case .success(let data):
+                                quizzes = data.shuffled()
+                            case .failure(let error):
+                                print(error)
+                            }
+                        }
+                    }
+                    .margin(top: 40)
                 }
-                // VStack
             }
-            // ZStack
         }
-        // GeometryReader
-        // 커스텀 뒤로가기 버튼 만들기
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: Button(action: {
-            self.mode.wrappedValue.dismiss()
-        }, label: {
-            Image(systemName: "arrow.left")
-                .font(Font.body.weight(.bold))
-                .tint(.black)
-        }))
-        .gesture(DragGesture().updating($dragOffSet, body: { (value, _, _) in
-            if value.startLocation.x < 20 && value.translation.width > 100 {
-                self.mode.wrappedValue.dismiss()
-            }
-        }))
         .preferredColorScheme(.light)
+        .navigationBarBackButtonHidden()
+    }
+
+    func generateAnswerText(index: Int, geometry: GeometryProxy) -> some View {
+        Text(quizzes[score].answers[index].text)
+            .fontWeight(.black)
+            .frame(minWidth: geometry.size.width * 0.6)
+            .font(.system(size: 20))
+            .padding(EdgeInsets(top: 20, leading: 40, bottom: 20, trailing: 40))
+            .background(.white)
+            .cornerRadius(80, antialiased: true)
+            .onTapGesture {
+                if quizzes[score].answers[index].isCorrect {
+                    score += 1
+                } else {
+                    answer = quizzes[score].answers[index == 0 ? 1 : 0].text
+                    description = quizzes[score].description
+                    isQuizEnded = true
+                }
+
+                if score == quizzes.count {
+                    isQuizEnded = true
+                }
+
+                randomBool = Bool.random()
+            }
     }
 
 }
 
 struct QuizView_Previews: PreviewProvider {
     static var previews: some View {
-        QuizView(isNavigationLinkActive: .constant(true))
+        QuizView()
     }
 }
